d6503bb2c6d8413563d445fc8ea31cfff997a017
Florencia <florenciamendez93@gmail.com>
2019-02-12 20:23:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var dotenv = require("dotenv");
+
+dotenv.config();
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+
+//Usuarios
+
+app.get("/usuarios", function(req, res) {
+  res.status(200).send("Todos los usuarios");
+});
+
+app.get("/usuarios/:id", function(req, res) {
+  res.status(200).send("Obtener un usuario por el id");
+});
+
+app.post("/usuarios", function(req, res) {
+  res.status(200).send("Agrega un nuevo usuario");
+});
+
+app.put("/usuarios/:id", function(req, res) {
+  res.status(200).send("Agrega un nuevo usuario");
+});
+
+app.delete("/usuarios/id", function(req, res) {
+  res.status(200).send("Agrega un nuevo usuario");
+});
+
+app.listen(port, function() {
+  console.log("Running app on port" + port);
+});
